921a231b9fb57d91d54b4ab0db4d6883deb567f0
waneefatima <waneefatima1568@gmail.com>
2025-09-04 12:00:16 +0530

    Add files via upload

---
+++ FTGS3.py
@@ -0,0 +1,18 @@
+def goto1():
+    print(num1+num2)
+def goto2():
+    print(num1-num2)
+def goto3():
+    print(num1/num2)
+num1 =int(input("enter your first number "))
+num2 =int(input("enter your second number "))
+
+res =str(input("enter the calculations you would like to do"))
+if res == "addition" :
+    goto1()
+elif res == "subtraction":
+    goto2()
+elif res == "division" :
+    goto3()
+elif res == "multiplication":
+    print (num1/num2)
